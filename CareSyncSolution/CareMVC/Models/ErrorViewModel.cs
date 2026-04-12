12b231f453e4f079c3e57492865774f5d4028b8a
Ahmed <fnr5541@gmail.com>
2026-04-12 07:04:56 +0300

    chore: setup files

---
+++ CareSyncSolution/CareMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CareMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
